eb7a4edba9d3f967c735f5711a57c4b603c8a7a2
Alexander Bugge Stage <abs@stubbe.dev>
2026-05-30 17:20:51 +0200

    chore: bump flake.nix to v2.5.1

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
 
         treeman = pkgs.buildGoModule {
           pname = "treeman";
-          version = "2.5.0";
+          version = "2.5.1";
           src = ./.;
           # buildGoModule fetches Go deps through the module proxy and
           # hashes the resulting vendor tree; `vendorHash` pins that
@@ -35,7 +35,7 @@
           ldflags = [
             "-s"
             "-w"
-            "-X github.com/stubbedev/treeman/internal/version.Version=2.5.0"
+            "-X github.com/stubbedev/treeman/internal/version.Version=2.5.1"
           ];
           doCheck = true;
         };
